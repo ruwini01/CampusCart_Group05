49d88f8f1aa825aec8439f89054ba8c5bf96e823
ruwini01 <12345tharanga12345@gmail.com>
2024-11-13 08:53:07 +0530

    Frontend & Backend Setup Done

---
+++ frontend/app/index.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text className='bg-red-500 font-semibold w-60 h-10'>Hi Ruwini!!</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
