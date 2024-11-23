e20e49c79a3cdc806970deac9909c05047a716bd
ruwini01 <12345tharanga12345@gmail.com>
2024-11-23 10:38:16 +0530

    Copied files from development/main branch

--- frontend/app/(auth)/signup_verify.jsx
+++ frontend/app/(auth)/signup_verify.jsx
@@ -1,17 +1,19 @@
 import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Link } from 'expo-router'
 
-const SavedItems = () => {
+function SignUpVerify() {
   return (
     <SafeAreaView>
       <ScrollView>
         <View>
-          <Text>Saved Items</Text>
+          <Text>Verify Email Page</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
   )
 }
 
-export default SavedItems
+export default SignUpVerify
+
