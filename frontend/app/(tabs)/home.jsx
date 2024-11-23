d301c23b07ba41157d896c3fd38bb45912f521c4
ruwini01 <12345tharanga12345@gmail.com>
2024-11-23 18:21:11 +0530

    Pages Added

--- frontend/app/(tabs)/home.jsx
+++ frontend/app/(tabs)/home.jsx
@@ -1,17 +1,19 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView , TouchableOpacity} from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import IconButton from '../../components/IconButton'
 import { Link } from 'expo-router'
+import { useRouter } from 'expo-router';
 
 const Home = () => {
+
   return (
     <SafeAreaView>
       <ScrollView>
         <View>
           <Text>Home</Text>
           {/* 3 navigation icon */}
-          <Link href='/buy_and_sell_home' className='text-blue-700'><IconButton/></Link>
+          <IconButton/>
           <Link href='/lost_and_found_home' className='text-blue-700'><IconButton/></Link>
           <Link href='/bording_home' className='text-blue-700'><IconButton/></Link>
         </View>
---
+++ frontend/app/buyandsellhome.jsx
@@ -0,0 +1,19 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Link } from 'expo-router'
+
+
+function BuyAndSellHome() {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>Buy and Sell home</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default BuyAndSellHome
