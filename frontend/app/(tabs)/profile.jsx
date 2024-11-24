4404b99ff2f9832d8b25a95a1dc6b24f11f8685b
Sachintha <125460323+sachinthapramod@users.noreply.github.com>
2024-11-24 17:27:24 +0530

    test

--- frontend/app/(tabs)/profile.jsx
+++ frontend/app/(tabs)/profile.jsx
@@ -1,7 +1,7 @@
 import { View, Text, ScrollView } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import ProductPost from '../../components/productCard/ProductPost'; // Import the ProductPost component
+// import ProductPost from '../../components/productCard/ProductPost'; // Import the ProductPost component
 
 // Image for the post
 const imgebed = require('../../assets/images/bed.jpg');
