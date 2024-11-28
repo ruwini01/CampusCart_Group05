d5daf0105c266b93d92f05e99dc8c7e2a1426715
ruwini01 <12345tharanga12345@gmail.com>
2024-11-28 10:36:32 +0530

    mongoDB Connected with backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,20 @@
-//MongoDB passkey:LKbFgfF3GnQvRQD2
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const port = 8080;
+
+
+const DB_PATH = "mongodb+srv://campuscart05:hCvRp64pRLfIrgfi@cluster0.j4rvj.mongodb.net/campuscart"; 
+
+mongoose.connect(DB_PATH, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("DB Connected Successfully");
+
+        app.listen(port, () => {
+            console.log(`Server running at port:${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error("DB Connection Error: ", err.message);
+        process.exit(1); 
+    });
