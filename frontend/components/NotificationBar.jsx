e41b87c6dead7db013c9062e9405705a75b948ab
ruwini01 <12345tharanga12345@gmail.com>
2024-12-06 12:15:01 +0530

    Notification section updated

--- frontend/components/NotificationBar.jsx
+++ frontend/components/NotificationBar.jsx
@@ -1,10 +1,20 @@
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import {images } from '../constants'
 
-function NotificationBar() {
+const NotificationBar = () => {
   return (
-    <div>
-      
-    </div>
+    <View className='clex flex-row w-full bg-white h-16 rounded-2xl items-center mb-2'>
+      <View>
+        <Image
+         source={images.umbrella}
+         className='ml-2 w-12 h-12 rounded-full'
+         resizeMode='contain'/>
+      </View>
+      <View>
+        <Text className='ml-4'>User123 just post an found umbrella</Text>
+      </View>
+    </View>
   )
 }
 
