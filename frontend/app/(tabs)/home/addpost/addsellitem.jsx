230bf20e52352bc86d7eba1806b47dca2d757dc3
ruwini01 <12345tharanga12345@gmail.com>
2024-12-06 12:14:26 +0530

    Update addsellitem.jsx

--- frontend/app/(tabs)/home/addpost/addsellitem.jsx
+++ frontend/app/(tabs)/home/addpost/addsellitem.jsx
@@ -4,10 +4,25 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import Checkbox from 'expo-checkbox';
 import FromField from '../../../../components/FromField';
 import RadioButtonGroup, { RadioButtonItem } from "expo-radio-button";
+import {Divider} from 'react-native-paper'
+import AddButton from '../../../../components/AddButton';
+import { router } from 'expo-router';
+
 
 const AddSellItem = () => {
   const [checkedItems, setCheckedItems] = useState({});
   const [current, setCurrent] = useState("used");
+  const [negotiable, setNegotiable] = useState(false);
+  const [sap, setSap] = useState(false);
+  const [hidephoneno , setHidephoneno] = useState(false);
+
+  
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = ()=>{
+      router.replace('/home')
+    }
+  
 
   const toggleCheckbox = (item) => {
     setCheckedItems((prev) => ({
@@ -31,14 +46,10 @@ const AddSellItem = () => {
 
   return (
     <SafeAreaView>
-      <Text className=" mx-6 font-semibold text-xl">Sell an Item</Text>
+      <Text className="mx-6 font-semibold text-xl">Sell an Item</Text>
       <ScrollView>
-        
         <View className="mx-6">
-          
           <Text className="font-normal text-base mt-6">Select a category</Text>
-
-      
           <View className="flex-row flex-wrap justify-between mt-6 pl-6">
             {categories.map((category, index) => (
               <View
@@ -56,36 +67,81 @@ const AddSellItem = () => {
           </View>
 
           <Text className="font-normal text-base mt-6 pb-1">Add Location</Text>
-          <FromField otherStyles='w-full'/>
+          <FromField otherStyles="w-full" />
 
           <Text className="font-normal text-base mt-6 pb-1">Item Name</Text>
-          <FromField otherStyles='w-full'/>
+          <FromField otherStyles="w-full" />
 
           <Text className="font-normal text-base mt-6 pb-1">Condition</Text>
-          <RadioButtonGroup
+          <View className="flex-row items-center gap-5">
+            <RadioButtonGroup
               selected={current}
               onSelected={(value) => setCurrent(value)}
               radioBackground="#0D7C66"
               size={16}
-      >
-        <RadioButtonItem value="used" label="Used" />
-        <RadioButtonItem value="brandnew" label="Brand New"/>
-      </RadioButtonGroup>
+            >
+              <RadioButtonItem value="used" label="Used" />
+              <RadioButtonItem value="brandnew" label="Brand New" />
+            </RadioButtonGroup>
+          </View>
 
-      <Text className="font-normal text-base mt-6 pb-1">Brand</Text>
-      <FromField otherStyles='w-full'/>
+          <Text className="font-normal text-base mt-6 pb-1">Brand</Text>
+          <FromField otherStyles="w-full" />
 
-      <Text className="font-normal text-base mt-6 pb-1">Description</Text>
+          <Text className="font-normal text-base mt-6 pb-1">Description</Text>
+          {/* Change This field to a description box (text area) */}
+          <FromField otherStyles="w-full h-[100]" />
 
-      {/* Change This field to a description box (text area) */}
-      <FromField otherStyles='w-full h-[100]'/>
+          <Text className="font-normal text-base mt-6 pb-1">Price</Text>
+          <FromField otherStyles="w-full" />
 
-      <Text className="font-normal text-base mt-6 pb-1">Price</Text>
-      <FromField otherStyles='w-full'/>
+          <Text className="font-normal text-base mt-6 pb-1">Original Price (If used)</Text>
+          <FromField otherStyles="w-full" />
 
-      <Text className="font-normal text-base mt-6 pb-1">Original Price</Text>
-      <FromField otherStyles='w-full'/>
-      <Checkbox value='negotiable'/><Text>Negotiable</Text>
+          <View className="flex-row items-center gap-5 mt-2 mx-1">
+            <Checkbox
+              value={negotiable}
+              onValueChange={() => setNegotiable(!negotiable)}
+              color={negotiable ? '#0D7C66' : '#0D7C66'}
+            />
+            <Text>Negotiable</Text>
+          </View>
+          <Text className="font-normal text-base mt-6 pb-1">Add upto 3 Photos</Text>
+          <Divider className='my-8'/>
+
+          <Text className="font-semibold text-lg pb-6">Contact Details</Text>
+         
+          <View className="flex-row items-center gap-5 mt-2 mx-1">
+            <Checkbox
+              value={sap}
+              onValueChange={() => setSap(!sap)}
+              color={sap ? '#0D7C66' : '#0D7C66'}
+            />
+            <Text>Same as My Profile</Text>
+          </View>
+          <Text className="font-normal text-base mt-6 pb-1">Name</Text>
+          <FromField otherStyles="w-full" />
+
+          <Text className="font-normal text-base mt-6 pb-1">Email</Text>
+          <FromField otherStyles="w-full" />
+
+          <Text className="font-normal text-base mt-6 pb-1">Telephone</Text>
+          <FromField otherStyles="w-full" />
+
+          <View className="flex-row items-center gap-5 mt-2 mx-1">
+            <Checkbox
+              value={hidephoneno}
+              onValueChange={() => setHidephoneno(!hidephoneno)}
+              color={hidephoneno ? '#0D7C66' : '#0D7C66'}
+            />
+            <Text>Hide Phone Number</Text>
+          </View>
+          <AddButton
+            handlePress={submit}
+            containerStyles="mt-7"
+            fontStyle="Montserrat_600SemiBold"
+            isLoading={isSubmitting}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
