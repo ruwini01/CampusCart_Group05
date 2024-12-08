58c16cafa69a9581f2973b031556a2524317eeb7
NifraAH <fathinifni@gmail.com>
2024-12-08 09:09:51 +0530

    checkbox imported

--- frontend/app/(tabs)/home/addpost/addbordinghouse.jsx
+++ frontend/app/(tabs)/home/addpost/addbordinghouse.jsx
@@ -1,6 +1,7 @@
 import { View, Text, ScrollView } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Checkbox from 'expo-checkbox';
 
 const AddBordingHouse = () => {
   return (
