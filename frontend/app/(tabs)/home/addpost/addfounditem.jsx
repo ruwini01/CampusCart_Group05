b7d2449f735c994fc8dc0506cd7fdea0229a96b6
NifraAH <fathinifni@gmail.com>
2024-12-08 11:06:42 +0530

    found item updated

--- frontend/app/(tabs)/home/addpost/addfounditem.jsx
+++ frontend/app/(tabs)/home/addpost/addfounditem.jsx
@@ -1,5 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Checkbox from 'expo-checkbox';
+import FromField from '../../../../components/FromField';
+import RadioButtonGroup, { RadioButtonItem } from "expo-radio-button";
+import {Divider} from 'react-native-paper'
+import AddButton from '../../../../components/AddButton';
+import { router } from 'expo-router';
 
 const AddFoundItem = () => {
   return (
--- frontend/app/(tabs)/home/addpost/addlostitem.jsx
+++ frontend/app/(tabs)/home/addpost/addlostitem.jsx
@@ -1,5 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Checkbox from 'expo-checkbox';
+import FromField from '../../../../components/FromField';
+import RadioButtonGroup, { RadioButtonItem } from "expo-radio-button";
+import {Divider} from 'react-native-paper'
+import AddButton from '../../../../components/AddButton';
+import { router } from 'expo-router';
 
 const AddLostItem = () => {
   const [checkedItems, setCheckedItems] = useState({});
@@ -37,9 +44,106 @@ const AddLostItem = () => {
   ];
 
   return (
-    <View>
-      <Text>AddLostItem</Text>
-    </View>
+    <SafeAreaView>
+      <Text className="mx-6 font-semibold text-xl">Post an Lost Item</Text>
+      <ScrollView>
+        <View className="mx-6">
+          <Text className="font-normal text-base mt-6">Select a category</Text>
+          <View className="flex-row flex-wrap justify-between mt-6 pl-6">
+            {categories.map((category, index) => (
+              <View
+                key={index}
+                className="w-[48%] flex-row items-center gap-5 mb-4"
+              >
+                <Checkbox
+                  value={checkedItems[category] || false}
+                  onValueChange={() => toggleCheckbox(category)}
+                  color={checkedItems[category] ? '#0D7C66' : '#0D7C66'}
+                />
+                <Text>{category}</Text>
+              </View>
+            ))}
+          </View>
+
+          <Text className="font-normal text-base mt-6 pb-1">Add Location</Text>
+          <FromField otherStyles="w-full" />
+
+          <Text className="font-normal text-base mt-6 pb-1">Item Name</Text>
+          <FromField otherStyles="w-full" />
+
+          <Text className="font-normal text-base mt-6 pb-1">Condition</Text>
+          <View className="flex-row items-center gap-5">
+            <RadioButtonGroup
+              selected={current}
+              onSelected={(value) => setCurrent(value)}
+              radioBackground="#0D7C66"
+              size={16}
+            >
+              <RadioButtonItem value="used" label="Used" />
+              <RadioButtonItem value="brandnew" label="Brand New" />
+            </RadioButtonGroup>
+          </View>
+
+          <Text className="font-normal text-base mt-6 pb-1">Brand</Text>
+          <FromField otherStyles="w-full" />
+
+          <Text className="font-normal text-base mt-6 pb-1">Description</Text>
+          {/* Change This field to a description box (text area) */}
+          <FromField otherStyles="w-full h-[100]" />
+
+          <Text className="font-normal text-base mt-6 pb-1">Price</Text>
+          <FromField otherStyles="w-full" />
+
+          <Text className="font-normal text-base mt-6 pb-1">Original Price (If used)</Text>
+          <FromField otherStyles="w-full" />
+
+          <View className="flex-row items-center gap-5 mt-2 mx-1">
+            <Checkbox
+              value={negotiable}
+              onValueChange={() => setNegotiable(!negotiable)}
+              color={negotiable ? '#0D7C66' : '#0D7C66'}
+            />
+            <Text>Negotiable</Text>
+          </View>
+          <Text className="font-normal text-base mt-6 pb-1">Add upto 3 Photos</Text>
+          <Divider className='my-8'/>
+
+          <Text className="font-semibold text-lg pb-6">Contact Details</Text>
+         
+          <View className="flex-row items-center gap-5 mt-2 mx-1">
+            <Checkbox
+              value={sap}
+              onValueChange={() => setSap(!sap)}
+              color={sap ? '#0D7C66' : '#0D7C66'}
+            />
+            <Text>Same as My Profile</Text>
+          </View>
+          <Text className="font-normal text-base mt-6 pb-1">Name</Text>
+          <FromField otherStyles="w-full" />
+
+          <Text className="font-normal text-base mt-6 pb-1">Email</Text>
+          <FromField otherStyles="w-full" />
+
+          <Text className="font-normal text-base mt-6 pb-1">Telephone</Text>
+          <FromField otherStyles="w-full" />
+
+          <View className="flex-row items-center gap-5 mt-2 mx-1">
+            <Checkbox
+              value={hidephoneno}
+              onValueChange={() => setHidephoneno(!hidephoneno)}
+              color={hidephoneno ? '#0D7C66' : '#0D7C66'}
+            />
+            <Text>Hide Phone Number</Text>
+          </View>
+          <AddButton
+            handlePress={submit}
+            containerStyles="mt-7"
+            fontStyle="Montserrat_600SemiBold"
+            isLoading={isSubmitting}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
