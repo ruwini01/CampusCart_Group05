1a9a41010d3fa0f638d6869382c5a25139d2d7c8
ruwini01 <12345tharanga12345@gmail.com>
2024-12-08 22:08:48 +0530

    Multer storage engine endpoint added

--- backend/index.js
+++ backend/index.js
@@ -9,13 +9,34 @@ const port = process.env.PORT;
 
 mongoose.connect(process.env.DB_PATH, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log("DB Connected Successfully");
-
-        app.listen(port, () => {
-            console.log(`Server running at port:${port}`);
-        });
+        console.log("DB Connected to MongoBD Successfully");
     })
     .catch((err) => {
-        console.error("DB Connection Error: ", err.message);
+        console.error("Error connecting to mongoDB: ", err.message);
         process.exit(1); 
     });
+
+
+app.listen(port, () => {
+    console.log(`Server running at port:${port}`);
+});
+
+
+// Image Storage Engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+});
+const upload = multer({ storage: storage });
+
+// Creating upload endpoint for images
+app.use('/images', express.static('upload/images'));
+
+app.post("/upload", upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    });
+});
