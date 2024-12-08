19f26ce5f1ad896372fbc402a56c1511589ef39e
NifraAH <fathinifni@gmail.com>
2024-12-08 11:07:19 +0530

    found item page updated

--- frontend/app/(tabs)/home/addpost/addfounditem.jsx
+++ frontend/app/(tabs)/home/addpost/addfounditem.jsx
@@ -9,6 +9,40 @@ import AddButton from '../../../../components/AddButton';
 import { router } from 'expo-router';
 
 const AddFoundItem = () => {
+  const [checkedItems, setCheckedItems] = useState({});
+  const [current, setCurrent] = useState("used");
+  const [negotiable, setNegotiable] = useState(false);
+  const [sap, setSap] = useState(false);
+  const [hidephoneno , setHidephoneno] = useState(false);
+
+  
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = ()=>{
+      router.replace('/home')
+    }
+  
+
+  const toggleCheckbox = (item) => {
+    setCheckedItems((prev) => ({
+      ...prev,
+      [item]: !prev[item],
+    }));
+  };
+
+  const categories = [
+    'Furniture',
+    'Electronics',
+    'Fashion',
+    'Education',
+    'Appliances',
+    'Essentials',
+    'Vehicles',
+    'Sports Equipment',
+    'Clothing',
+    'Other',
+  ];
+
   return (
     <View>
       <Text>AddFoundItem</Text>
