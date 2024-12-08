4a0575891a6f8d37996fc3ca40f39d7263307566
NifraAH <fathinifni@gmail.com>
2024-12-08 10:30:39 +0530

    submit function created

--- frontend/app/(tabs)/home/addpost/addbordinghouse.jsx
+++ frontend/app/(tabs)/home/addpost/addbordinghouse.jsx
@@ -6,8 +6,20 @@ import FromField from '../../../../components/FromField';
 import RadioButtonGroup, { RadioButtonItem } from "expo-radio-button";
 import {Divider} from 'react-native-paper'
 import AddButton from '../../../../components/AddButton';
+import { router } from 'expo-router';
 
 const AddBordingHouse = () => {
+  const [checkedItems, setCheckedItems] = useState({});
+  const [current, setCurrent] = useState("used");
+  const [negotiable, setNegotiable] = useState(false);
+  const [sap, setSap] = useState(false);
+  const [hidephoneno , setHidephoneno] = useState(false);
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = ()=>{
+    router.replace('/home')
+  }
   return (
     <View>
       <Text>AddBordingHouse</Text>
