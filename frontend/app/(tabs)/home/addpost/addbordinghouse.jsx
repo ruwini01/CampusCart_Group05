2b749d039bd6f638c5dd12845e1ae6ed327e19e5
NifraAH <fathinifni@gmail.com>
2024-12-08 10:26:14 +0530

    FromField imported

--- frontend/app/(tabs)/home/addpost/addbordinghouse.jsx
+++ frontend/app/(tabs)/home/addpost/addbordinghouse.jsx
@@ -2,6 +2,7 @@ import { View, Text, ScrollView } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Checkbox from 'expo-checkbox';
+import FromField from '../../../../components/FromField';
 
 const AddBordingHouse = () => {
   return (
