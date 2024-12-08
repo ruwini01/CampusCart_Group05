f8efa1d8184d56d6a5b14a925bd9427e38536ec4
NifraAH <fathinifni@gmail.com>
2024-12-08 10:28:15 +0530

    AddButton imported

--- frontend/app/(tabs)/home/addpost/addbordinghouse.jsx
+++ frontend/app/(tabs)/home/addpost/addbordinghouse.jsx
@@ -5,6 +5,7 @@ import Checkbox from 'expo-checkbox';
 import FromField from '../../../../components/FromField';
 import RadioButtonGroup, { RadioButtonItem } from "expo-radio-button";
 import {Divider} from 'react-native-paper'
+import AddButton from '../../../../components/AddButton';
 
 const AddBordingHouse = () => {
   return (
