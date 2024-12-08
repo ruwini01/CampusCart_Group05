f5276b0c460dbeed7623235f79948bf443ea4704
NifraAH <fathinifni@gmail.com>
2024-12-08 09:09:29 +0530

    safeareaview imported

--- frontend/app/(tabs)/home/addpost/addbordinghouse.jsx
+++ frontend/app/(tabs)/home/addpost/addbordinghouse.jsx
@@ -1,5 +1,7 @@
 import { View, Text, ScrollView } from 'react-native';
 import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 const AddBordingHouse = () => {
   return (
     <View>
