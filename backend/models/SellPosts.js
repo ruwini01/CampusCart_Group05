b4f8e2da42d0f5cd0bfbfc4f5d4ee4d4809e8dee
Sachintha <125460323+sachinthapramod@users.noreply.github.com>
2024-12-09 22:11:03 +0530

    sellPost completed

---
+++ backend/models/SellPosts.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const sellPostsSchema = new Schema({
+    postId:{
+        type:Number,
+        required:true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    itemname: {
+        type: String,
+        required: true
+    },
+    condition:{
+        type:String,
+        required: true
+    },
+    brand: {
+        type: String,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    originalprice: {
+        type: Number,
+    },
+    isnagotiable:{
+        type:Boolean,
+        default:false
+    },
+    images:{
+        type:Array,
+    },
+    contact:{
+        type:Object,
+        required:true
+    },
+    hidephoneno:{
+        type:Boolean,
+        default:false
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+});
+
+const SellPosts = mongoose.model('sellposts', sellPostsSchema);
+
+
+usersSchema.pre('save', async function(next) {
+    if (this.isNew) {
+        const lastUser = await Users.findOne().sort({ userId: -1 });
+        this.userId = lastUser ? lastUser.userId + 1 : 1;
+    }
+    next();
+});
+
+module.exports = SellPosts;
