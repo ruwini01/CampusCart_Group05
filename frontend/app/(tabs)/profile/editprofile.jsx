67914c550af8702d475c204d51852494d06582e5
NifraAH <fathinifni@gmail.com>
2024-12-10 20:27:24 +0530

    address component edited

--- frontend/app/(tabs)/profile/editprofile.jsx
+++ frontend/app/(tabs)/profile/editprofile.jsx
@@ -23,9 +23,68 @@ const EditProfile = () => {
   };
 
   return (
-    <View>
-      <Text>EditProfile</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View className="mx-6">
+          <Text className="font-semibold text-xl mt-6">Edit Profile</Text>
+
+          <View style={{ 
+
+            borderRadius: 50, // Make it round
+            //overflow: 'hidden', // Ensure the image fits within the round shape
+            marginBottom: 10 
+          }}>
+            <ImageUploadBox
+              index={0}
+              imageUri={profileImage}
+              onImageSelect={handleImageSelect}
+            />
+          </View>
+
+          <Text className="font-normal text-base mt-6 pb-1">Name</Text>
+          <FromField
+            value={name}
+            handleChangeText={setName}
+            placeholder="Enter your name"
+          />
+
+          <Text className="font-normal text-base mt-6 pb-1">Email</Text>
+          <FromField
+            value={email}
+            handleChangeText={setEmail}
+            placeholder="Enter your email"
+          />
+
+          <Text className="font-normal text-base mt-6 pb-1">Telephone</Text>
+          <FromField
+            value={telephone}
+            handleChangeText={setTelephone}
+            placeholder="Enter your telephone"
+          />
+          <Text className="font-normal text-base mt-6 pb-1">Address</Text>
+          <FromField
+            value={address}
+            handleChangeText={setAddress}
+            placeholder="Enter your address"
+          />
+
+          <View className="flex-row justify-between mt-6">
+            <AddButton
+              handlePress={() => console.log('Edit button pressed')}
+              containerStyles="bg-gray-400"
+              fontStyle="Montserrat_600SemiBold"
+              textStyles="text-white"
+            />
+            <AddButton
+              handlePress={handleSave}
+              containerStyles="bg-[#0D7C66]"
+              fontStyle="Montserrat_600SemiBold"
+              textStyles="text-white"
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
