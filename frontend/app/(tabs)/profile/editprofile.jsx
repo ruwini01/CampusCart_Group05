924e003f13518eebeb94256ee7070d7f51e8a90b
NifraAH <fathinifni@gmail.com>
2024-12-10 20:15:28 +0530

    SafeAreaView imported

--- frontend/app/(tabs)/profile/editprofile.jsx
+++ frontend/app/(tabs)/profile/editprofile.jsx
@@ -1,6 +1,6 @@
 import { View, Text, ScrollView } from 'react-native';
 import React, { useState } from 'react';
-
+import { SafeAreaView } from 'react-native-safe-area-context';
 const EditProfile = () => {
   return (
     <View>
