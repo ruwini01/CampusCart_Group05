3e5cce3b3e6adbd94b2ab4566a31e2c485ddde1b
NifraAH <fathinifni@gmail.com>
2024-12-10 20:16:40 +0530

    AddButton imported

--- frontend/app/(tabs)/profile/editprofile.jsx
+++ frontend/app/(tabs)/profile/editprofile.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FromField from '../../../components/FromField';
 import ImageUploadBox from '../../../components/ImageUploadBox';
+import AddButton from '../../../components/AddButton';
 const EditProfile = () => {
   return (
     <View>
