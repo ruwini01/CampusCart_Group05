508950d10203c34cbf3870a9bf951c30e1316d12
NifraAH <fathinifni@gmail.com>
2024-12-10 20:16:16 +0530

    ImageUploadBox imported

--- frontend/app/(tabs)/profile/editprofile.jsx
+++ frontend/app/(tabs)/profile/editprofile.jsx
@@ -2,6 +2,7 @@ import { View, Text, ScrollView } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FromField from '../../../components/FromField';
+import ImageUploadBox from '../../../components/ImageUploadBox';
 const EditProfile = () => {
   return (
     <View>
