9e586cf57ac5f6358152376855bac553388e64f9
NifraAH <fathinifni@gmail.com>
2024-12-10 20:15:53 +0530

    FromField imported

--- frontend/app/(tabs)/profile/editprofile.jsx
+++ frontend/app/(tabs)/profile/editprofile.jsx
@@ -1,6 +1,7 @@
 import { View, Text, ScrollView } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import FromField from '../../../components/FromField';
 const EditProfile = () => {
   return (
     <View>
