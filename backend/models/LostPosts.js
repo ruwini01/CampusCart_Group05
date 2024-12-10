0917372a23ea5df7e42c1613d9e66692ac1ba64e
Tharmi01 <tharmihansenthur01@gmail.com>
2024-12-10 22:10:13 +0530

    lost posts model created

---
+++ backend/models/LostPosts.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const lostPostsSchema = new Schema({
+  postId: {
+    type: String,
+    required: true,
+  },
+  itemname: {
+    type: String,
+    required: true,
+  },
+  lostdate: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: Array,
+  },
+  contact: {
+    type: Object,
+    required: true,
+  },
+  hidephoneno: {
+    type: Boolean,
+    default: false,
+  },
+  status: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const LostPosts = mongoose.model("lostPosts", lostPostsSchema);
+
+module.exports = LostPosts;
