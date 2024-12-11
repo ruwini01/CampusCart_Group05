5535e7732819350f2cbf2f9278e71138ab397e9e
Kaweesha-Malshani <537tharu@gmail.com>
2024-12-11 16:47:44 +0530

    edit sellroutes

--- backend/routes/sellRoutes.js
+++ backend/routes/sellRoutes.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const addSellPost = require('../controllers/addSellPostController');
 const addBuyPost = require('../controllers/addBuyPostController');
+const listSellPosts = require('../controllers/listSellPostsController');
 const router = express.Router();
 
 
 router.post('/sell', addSellPost);
 
-
 router.post('/buy', addBuyPost);
+
+router.get('/sell', listSellPosts);
 
 module.exports = router;
