7ee6066452b8f40374896c271a814bbfb2e7b7eb
Tharmi01 <tharmihansenthur01@gmail.com>
2025-01-02 23:09:28 +0530

    Create appNavigation.jsx

---
+++ frontend/navigation/appNavigation.jsx
@@ -0,0 +1,20 @@
+import React,{useEffect,useState} from "react";
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen.js';
+import OnboardingScreen from "../screens/OnboardingScreen.js";
+
+const Stack=createNativeStackNavigator();
+
+export default function AppNavigation(){
+
+    return(
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='Onboarding'>
+                <Stack.Screen name="Onboarding" options={{headerShown:false}} component={OnboardingScreen}/>
+                 <Stack.Screen name="Home" options={{headerShown:false}} component={HomeScreen}/>
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
