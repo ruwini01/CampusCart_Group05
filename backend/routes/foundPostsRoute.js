d28042ca8caded0a1212add78eb7b23ca84e2336
Tharmi01 <tharmihansenthur01@gmail.com>
2025-01-06 19:52:00 +0530

    addfoundpost route added

--- backend/routes/foundPostsRoute.js
+++ backend/routes/foundPostsRoute.js
@@ -2,8 +2,50 @@ const express = require('express');
 const router = express.Router();
 const {default: mongoose, Model} = require('mongoose');
 
-router.post('/addfoundpost', async(req, res)=>{
+router.post('/addfoundpost',AuthToken, async(req, res)=>{
+    const user=req.user;
 
+        const foundPostData = {
+            itemname: req.body.itemname,
+            founddate: req.body.founddate,
+            location: req.body.location,
+            description: req.body.description,
+            images: req.body.images,
+            contact: req.body.contact,
+            hidephoneno: req.body.hidephoneno,
+            status: 'searching for owner',
+        };
+
+        try
+        {
+            const newfoundPost = new FoundPosts(foundPostData);
+            const savedFoundPost = await newfoundPost.save();
+            try{
+                const postEntry = {
+                    userId: user._id,
+                    postId: savedFoundPost._id,
+                    category: 'found',
+                };
+        
+                const newPost = new Posts(postEntry);
+                await newPost.save();
+
+                res.status(201).json({
+                    success: true,
+                    message: 'found post added successfully!',
+                    foundPost: savedFoundPost,
+                    postEntry: newPost,
+                });
+            }
+            catch(error){
+                await FoundPosts.findByIdAndDelete(savedFoundPost._id);
+                res.status(500).json({ error: 'An error occurred while post entry.'+error });
+            }
+        }
+        catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'An error occurred while adding the found post.' });
+        }
 })
 
 
