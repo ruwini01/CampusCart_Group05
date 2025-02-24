6e4fb3581247adc393a17f82e1bc3ac6703ca49b
Kaweesha-Malshani <537tharu@gmail.com>
2025-02-24 09:33:56 +0530

    fixed error sellPostsRoute.js

--- backend/routes/sellPostsRoute.js
+++ backend/routes/sellPostsRoute.js
@@ -89,7 +89,7 @@ router.get('/listsellposts/:id', async(req, res)=>{
 
 
 
-router.delete('/removesellpost', async(req, res)=>{
+router.delete('/removesellpost/:postId',AuthToken, async(req, res)=>{
     try {
         const { postId } = req.params;
 
