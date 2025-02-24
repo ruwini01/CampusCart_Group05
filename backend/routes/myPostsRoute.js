5a2cc385cb1a38058d7b53a54434f2b28a198aef
Kaweesha-Malshani <537tharu@gmail.com>
2025-02-24 12:36:09 +0530

    add myPostsRoute/getAllposts

--- backend/routes/myPostsRoute.js
+++ backend/routes/myPostsRoute.js
@@ -22,3 +22,62 @@ router.get('/myposts', AuthToken, async (req, res) => {
         res.status(500).json({ error: 'An error occurred while fetching your posts.' });
     }
 });
+
+router.get('/getAllPosts', AuthToken, async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const allPosts = await Posts.find({ userId: userId });
+
+        let detailedPosts = [];
+
+        for (const post of allPosts) {
+            let postDetails;
+
+            switch (post.category) {
+                case 'sell':
+                    postDetails = await SellPosts.findById(post.postId);
+                    break;
+                case 'lost':
+                    postDetails = await LostPosts.findById(post.postId);
+                    break;
+                case 'found':
+                    postDetails = await FoundPosts.findById(post.postId);
+                    break;
+                case 'boarding':
+                    postDetails = await BoardingPosts.findById(post.postId);
+                    break;
+                default:
+                    continue;
+            }
+
+            if (postDetails) {
+                
+                detailedPosts.push({
+                    _id: postDetails._id,
+                    date: post.date,
+                    ...postDetails.toObject(),
+                });
+            }
+        }
+
+        res.status(200).json({
+            success: true,
+            posts: detailedPosts
+            
+        });
+
+    } catch (error) {
+        console.error('Error fetching user posts:', error);
+        res.status(500).json({
+            success: false,
+            message: 'Error fetching user posts',
+            error: error.message
+        });
+    }
+});
+
+
+
+
+
+module.exports = router;
