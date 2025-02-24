4d4f53839b85075baeefa9e10eecef741535ac8f
Kaweesha-Malshani <537tharu@gmail.com>
2025-02-24 13:27:34 +0530

    add sellPostsRoute/removesellpost

--- backend/routes/sellPostsRoute.js
+++ backend/routes/sellPostsRoute.js
@@ -5,6 +5,7 @@ const SellPosts = require('../models/SellPosts');
 const Posts = require('../models/posts');
 const Users = require('../models/Users');
 const jwt = require('jsonwebtoken');
+const AuthToken = require('../middleware/authToken');
 
 const JWT_SECRET = '#campusCartGroup05*';
 
@@ -89,9 +90,23 @@ router.get('/listsellposts/:id', async(req, res)=>{
 
 
 
-router.delete('/removesellpost', async(req, res)=>{
+router.delete('/removesellpost/:postId',AuthToken, async (req, res) => {
+    try {
+        const { postId } = req.params;
 
-})
+        const deletedPost = await SellPosts.findByIdAndDelete({_id:postId});
+        if (!deletedPost) {
+            return res.status(400).json({ success: false, message: 'Sell post not found.' });
+        }
+
+        await Posts.findOneAndDelete({ postId, category: 'found' });
+
+        res.status(200).json({ success: true, message: 'Sell post removed successfully.' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while removing the sell post.'+error });
+    }
+});
 
 
 router.put('/editsellpost', async(req, res)=>{
