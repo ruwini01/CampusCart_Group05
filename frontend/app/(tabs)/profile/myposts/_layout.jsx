8611966754ef7c0fde30c68af6a71ca3389531c6
Sachintha <125460323+sachinthapramod@users.noreply.github.com>
2025-02-26 21:05:46 +0530

    post editing and deleting backend routes connected with editlostitem.jsx

--- frontend/app/(tabs)/profile/myposts/_layout.jsx
+++ frontend/app/(tabs)/profile/myposts/_layout.jsx
@@ -9,11 +9,11 @@ export default function MyPostsLayout() {
       }}
     >
       <Stack.Screen name="index" options={{ title: '' }} />
-      <Stack.Screen name="editbordinghouse" options={{ title: ''}} />
+      <Stack.Screen name="editbordinghouse" options={{ title: '' }} />
       <Stack.Screen name="editfounditem" options={{ title: '' }} />
       <Stack.Screen name="editlostitem" options={{ title: '' }} />
       <Stack.Screen name="editsellitem" options={{ title: '' }} />
-  
+
     </Stack>
   );
 }
