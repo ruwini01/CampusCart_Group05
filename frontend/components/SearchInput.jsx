51011786d6fcd360cc8777f27878c0f81c5caf7a
Ruwini Tharanga <138703319+iamloshan@users.noreply.github.com>
2025-02-28 11:23:42 +0530

    Added state to update searchQuery

--- frontend/components/SearchInput.jsx
+++ frontend/components/SearchInput.jsx
@@ -1,23 +1,27 @@
-import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
-import { icons } from '../constants'
+import { View, TextInput, TouchableOpacity, Image } from 'react-native';
+import React from 'react';
+import { icons } from '../constants';
 
-
-const SearchInput = () => {
+const SearchInput = ({ searchQuery, setSearchQuery }) => {
   return (
-    <View className='flex-row items-center border-2 w-full h-14 px-4 rounded-3xl  border-[#1d816ddc]'>
-      <TextInput className="flex-1 font-normal text-base mt-[-8]"/>
+    <View className="flex-row items-center border-2 w-full h-14 px-4 rounded-3xl border-[#1d816ddc]">
+      <TextInput
+        className="flex-1 font-normal text-base"
+        placeholder="Search..."
+        value={searchQuery}
+        onChangeText={setSearchQuery} // Updates the search query state
+      />
 
       <TouchableOpacity>
         <Image
           source={icons.search}
-          className='w-5 h-5 mr-1'
-          resizeMethod='contain'
-          tintColor='#1d816ddc'
+          className="w-5 h-5 mr-1"
+          resizeMode="contain"
+          tintColor="#1d816ddc"
         />
       </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default SearchInput
+export default SearchInput;
