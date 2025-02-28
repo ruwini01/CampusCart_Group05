311619aa2647594d677eccb9c4b44603a1eb1d17
Ruwini Tharanga <138703319+iamloshan@users.noreply.github.com>
2025-02-28 11:41:00 +0530

    Update SearchInput and LostPosts to use searchQuery state

--- frontend/app/(tabs)/home/losthome.jsx
+++ frontend/app/(tabs)/home/losthome.jsx
@@ -1,13 +1,19 @@
-import { Text } from 'react-native';
-import React from 'react';
+import { Text, View } from 'react-native';
+import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import LostPosts from '../../../components/LostPosts';
+import SearchInput from '../../../components/SearchInput';
 
 const LostHome = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+
   return (
     <SafeAreaView className="flex-1">
-      <Text className="font-semibold text-xl mb-6 px-6">Lost items</Text>
-      <LostPosts />
+      <Text className="font-semibold text-xl mb-6 px-6">Lost Items</Text>
+      <View className="mb-6 px-6">
+        <SearchInput searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      </View>
+      <LostPosts searchQuery={searchQuery} />
     </SafeAreaView>
   );
 };
