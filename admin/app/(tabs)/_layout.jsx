a4557b248c60c4dbe7771b93e676861ea53a3c4b
Ruwini Tharanga <138703319+iamloshan@users.noreply.github.com>
2025-03-09 17:21:58 +0530

    Bottom Tab bar created with 4 tabs

---
+++ admin/app/(tabs)/_layout.jsx
@@ -0,0 +1,104 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import {Tabs, Redirect} from 'expo-router'
+import {icons} from '../../constants'
+
+const TabIcon = ({icon, color, name, focused}) => {
+    return (
+        <View className='items-center justify-center gap-2 w-20 pt-3'>
+            <Image
+                source={icon}
+                resizeMethod='contain'
+                tintColor={color}
+                className="w-5 h-5"
+            />
+            <Text className={`${focused? 'font-semibold' : 'font-normal'} text-xs`} style={{color:color}}>{name}</Text>
+        </View>
+    )
+}
+
+const TabsLayout = () => {
+  return (
+    <>
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarActiveTintColor: '#0D7C66',
+                tabBarInactiveTintColor: '#B7B7B7',
+                tabBarStyle:{
+                    borderTopWidth:'0.2',
+                    borderTopColor:'#0D7C66',
+                    height:84,
+                }
+            }}
+        >
+            <Tabs.Screen
+                name='home'
+                options={{
+                    title:'Home',
+                    headerShown:false,
+                    tabBarIcon: ({color, focused})=>(
+                        <TabIcon
+                            icon={icons.home}
+                            color={color}
+                            name="Home"
+                            focused={focused}
+                        />
+                    )
+                }}
+            />
+
+<Tabs.Screen
+                name='notifications'
+                options={{
+                    title:'Notifications',
+                    headerShown:false,
+                    tabBarIcon: ({color, focused})=>(
+                        <TabIcon
+                            icon={icons.notification}
+                            color={color}
+                            name="Notifications"
+                            focused={focused}
+                        />
+                    )
+                }}
+            />
+
+            <Tabs.Screen
+                name='email'
+                options={{
+                    title:'Emails',
+                    headerShown:false,
+                    tabBarIcon: ({color, focused})=>(
+                        <TabIcon
+                            icon={icons.email}
+                            color={color}
+                            name="Emails"
+                            focused={focused}
+                        />
+                    )
+                }}
+            />
+ 
+
+            <Tabs.Screen
+                name='profile'
+                options={{
+                    title:'Profile',
+                    headerShown:false,
+                    tabBarIcon: ({color, focused})=>(
+                        <TabIcon
+                            icon={icons.profile}
+                            color={color}
+                            name="Profile"
+                            focused={focused}
+                        />
+                    )
+                }}
+            />
+        </Tabs>
+    </>
+  )
+}
+
+export default TabsLayout
